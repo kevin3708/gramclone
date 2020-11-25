6ef3d426e14afea7aa277a6690ad2eb17d503cec
Kevin <kevinambundo3@gmail.com>
2020-11-25 18:26:17 +0300

    add changes

--- gramclone/views.py
+++ gramclone/views.py
@@ -198,7 +198,7 @@ def login(request):
         login(request, user)
         return redirect(request,'/')
     
-    return render(request, '/registration/login.html')
+    return render(request, 'registration/login.html')
         
 @login_required
 def logout(request):

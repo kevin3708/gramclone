dae8c5855d2e8b30ffbeaf80fe1b6ad6512fac55
Kevin <kevinambundo3@gmail.com>
2020-11-25 18:14:16 +0300

    add changes

--- gramclone/views.py
+++ gramclone/views.py
@@ -191,8 +191,8 @@ def search_results(request):
 
 
 def login(request):
-    username = request.POST.get['username']
-    password = request.POST.get['password']
+    username = request.POST.get('username')
+    password = request.POST.get('password')
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
